f62ba86f0d7774acacbb11043ffcc97e7679978b
Matt Lamont <m@lmnt.nz>
2023-05-17 18:36:52 +1200

    Introduce a `ColorStore` state object instead of using `getStaticProps`

---
+++ web-app/ColorStore.ts
@@ -0,0 +1,10 @@
+import colorData from '../scraper/colors.json';
+
+class ColorStore {
+  constructor() {
+  }
+
+  colors = colorData.colors;
+}
+
+export { ColorStore };
--- web-app/pages/index.tsx
+++ web-app/pages/index.tsx
@@ -1,22 +1,19 @@
+import { useState } from 'react';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
-import colorCatalog from '../../scraper/colors.json';
 import ColorCardList from '@/components/ColorCardList';
+import { ColorStore } from '@/ColorStore'
 import Stage from '@/components/Stage';
 
-export default function Home(props) {
+export default function Home() {
+  const [colorStore, _] = useState(new ColorStore())
+
   return (
     <DndProvider backend={ HTML5Backend }>
       <div className='flex flex-col h-screen'>
-        <ColorCardList colorCatalog={ props.colors } />
+        <ColorCardList colorCatalog={ colorStore.colors } />
         <Stage/>
       </div>
     </DndProvider>
   )
-}
-
-export async function getStaticProps(context) {
-  return {
-    props: colorCatalog
-  }
 }
